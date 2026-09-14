b532b85aefc346ce3ec45743116df26bcea401a0
Jitesh Soni <jiteshsoni3591@gmail.com>
2026-09-14 15:06:11 +0530

    fix managerOnly import in clientRoutes

--- routes/clientRoutes.js
+++ routes/clientRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const Client  = require('../models/Client');
 const CallLog = require('../models/CallLog');
-const { protect } = require('../middleware/auth');
+const { protect, managerOnly } = require('../middleware/auth');
 
 const router = express.Router();
 
